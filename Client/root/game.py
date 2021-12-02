b577b7fb32dc2977c86744b41eef950f03888332
Licymnius <licymnius@outlook.com>
2021-12-02 17:06:36 +0300

    Update game.py

--- Client/root/game.py
+++ Client/root/game.py
@@ -8,8 +8,6 @@
 			if systemSetting.GetSnowMode():
 				systemSetting.SetSnowMode(True)
 				background.EnableSnow(1, constInfo.ENVIRONMENT_SNOW)
-				background.SetEnvironmentData(1)
 			else:
 				systemSetting.SetSnowMode(False)
 				background.EnableSnow(0, constInfo.ENVIRONMENT_SNOW)
-				background.SetEnvironmentData(0)
